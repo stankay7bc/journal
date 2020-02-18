e6b286de894f6edd81a88367dd07fd2f6276fe00
Sten <s.light.post@gmail.com>
2020-02-18 13:14:13 +0700

    ui fix

--- js/index.js
+++ js/index.js
@@ -14,11 +14,24 @@ document.body.onload = event => {
   }).then(function(response) {
     //console.log(response);
     let result = `${response.reduce((html,post,index)=>{
-      return `${html}<li><span>${post.time}</span> <a href="page.html?post=${index}">
-      ${post.title}</a></li>`;
+      return `${html}<li>${postTitleView(post,index)}</li>`;
     },'<ul>')}</ul>`;
     
     document.querySelector('#posts').innerHTML = result;
   });
 
 };
+
+/**
+ * 
+ * @param {Object} post 
+ * @param {Number} index 
+ * @return {String} html-formatted string
+ */
+function postTitleView(post,index) {
+  let options = { year: 'numeric', month: 'long', day: 'numeric' };
+  return `<section>
+<div><a href="page.html?post=${index}">${post.title}</a></div>
+<div><span>${(new Date(post.time)).toLocaleDateString('ru',options)}</span></div>
+</section>`;
+}
